e0b7ae587a5f5becc9331f21e29ec1e93f60fd2a
Pavel Stano <stano@websupport.sk>
2015-11-14 13:50:26 +0100

    fix segfault in case of bufferevent_socket_connect fail immediately (fd limit ?)

--- mysql_mitm.c
+++ mysql_mitm.c
@@ -320,7 +320,7 @@ handle_auth_packet_from_client (struct bev_arg *bev_arg,
 
     bufferevent_setwatermark (bev_remote, EV_READ, 0, INPUT_BUFFER_LIMIT);
     bev_arg_remote->connecting = 1;
-
+    bev_arg_remote->destination = destination;
 
     if (bufferevent_socket_connect
         (bev_remote, (struct sockaddr *) &destination->sin,
@@ -375,7 +375,6 @@ handle_auth_packet_from_client (struct bev_arg *bev_arg,
         event_new (event_base, -1, 0, mysql_connect_timeout_cb,
                    bev_arg_remote);
     event_add (bev_arg_remote->connect_timer, &time);
-    bev_arg_remote->destination = destination;
 
     return 1;
 }
