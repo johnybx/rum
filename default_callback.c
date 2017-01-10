c1e4f3232ed7702d93f296fb6005bd027191a40c
Pavel Stano <pavel.stano@websupport.sk>
2017-01-10 13:08:48 +0100

    log error string

--- default_callback.c
+++ default_callback.c
@@ -161,7 +161,7 @@ event_callback (struct bufferevent *bev, short events, void *ptr)
                    (BEV_EVENT_ERROR | BEV_EVENT_EOF | BEV_EVENT_TIMEOUT)) {
             if (bev_arg->type == BEV_TARGET) {
                 if (events & BEV_EVENT_ERROR) {
-                    logmsg ("BEV_EVENT_ERROR dest: %s\n", bev_arg->destination->s);
+                    logmsg ("BEV_EVENT_ERROR dest: %s error: %s\n", bev_arg->destination->s, evutil_socket_error_to_string(evutil_socket_geterror(bufferevent_getfd(bev))));
                 } else if (events & BEV_EVENT_TIMEOUT) {
                     logmsg ("BEV_EVENT_TIMEOUT dest: %s\n", bev_arg->destination->s);
                 }
